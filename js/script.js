95a86e1bb09a619e0f3f2f463b6f0b6da1d606b0
yadimaksta <90519111+yadimaksta@users.noreply.github.com>
2022-06-08 01:13:41 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+  // ITEMS
+  $(".collection-block-sharp").click(function(){
+    $(this).addClass("black-color");
+    $(".collection-block-soft").removeClass("black-color");
+    $(".collection-block-power").removeClass("black-color");
+    $(".collection-block-air").removeClass("black-color");
+    $(".item-sharp").css("display","flex");
+    $(".item-soft").css("display","none");
+    $(".item-power").css("display","none");
+    $(".item-air").css("display","none");
+  });
+  $(".collection-block-soft").click(function(){
+    $(this).addClass("black-color");
+    $(".collection-block-sharp").removeClass("black-color");
+    $(".collection-block-power").removeClass("black-color");
+    $(".collection-block-air").removeClass("black-color");
+    $(".item-sharp").css("display","none");
+    $(".item-soft").css("display","flex");
+    $(".item-power").css("display","none");
+    $(".item-air").css("display","none");
+  });
+  $(".collection-block-power").click(function(){
+    $(this).addClass("black-color");
+    $(".collection-block-soft").removeClass("black-color");
+    $(".collection-block-sharp").removeClass("black-color");
+    $(".collection-block-air").removeClass("black-color");
+    $(".item-sharp").css("display","none");
+    $(".item-soft").css("display","none");
+    $(".item-power").css("display","flex");
+    $(".item-air").css("display","none");
+  });
+  $(".collection-block-air").click(function(){
+    $(this).addClass("black-color");
+    $(".collection-block-soft").removeClass("black-color");
+    $(".collection-block-power").removeClass("black-color");
+    $(".collection-block-sharp").removeClass("black-color");
+    $(".item-sharp").css("display","none");
+    $(".item-soft").css("display","none");
+    $(".item-power").css("display","none");
+    $(".item-air").css("display","flex");
+  });
+});
